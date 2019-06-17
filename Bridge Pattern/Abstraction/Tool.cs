821b4ee8939fe9fa35054ccf1155636ae44ddcd5
Thomas Micol <thomasmicol@gmail.com>
2019-06-17 18:07:47 +1200

    Added the tool base class for the abstraction side of the bridge pattern

---
+++ Bridge Pattern/Abstraction/Tool.cs
@@ -0,0 +1,22 @@
+﻿using Bridge_Pattern.Implementor;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bridge_Pattern.Abstraction
+{
+    class Tool
+    {
+        private IComboDeal _comboDeal;
+
+        public Tool(IComboDeal comboDeal)
+        {
+            _comboDeal = comboDeal;
+        }
+
+        public void PrintComboDeal()
+        {
+
+        }
+    }
+}
