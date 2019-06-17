3aca14a62f729320cc7088d7116716d4ab77289e
Thomas Micol <thomasmicol@gmail.com>
2019-06-17 18:38:05 +1200

    Bridge Pattern: Implemented the nails with hardhat implementor

--- Bridge Pattern/Implementor/NailsWithHardhat.cs
+++ Bridge Pattern/Implementor/NailsWithHardhat.cs
@@ -6,5 +6,32 @@ namespace Bridge_Pattern.Implementor
 {
     class NailsWithHardhat : IComboDeal
     {
+        public List<Item> _myItems { get; set; }
+
+        public NailsWithHardhat()
+        {
+            _myItems = new List<Item> {
+                new Item {
+                    Name = "Nails",
+                    Price = 0.20f,
+                    Description = "A pack of normal nails"
+                },
+                new Item {
+                    Name = "Golden Nails",
+                    Price = 50.40f,
+                    Description = "a pack of golden nails?!"
+                },
+                new Item {
+                    Name = "Hardhat",
+                    Price = 2.40f,
+                    Description = "A bog standard hardhat"
+                }
+            };
+        }
+        public List<Item> GetItems()
+        {
+            return _myItems;
+        }
+
     }
 }
