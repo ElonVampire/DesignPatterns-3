a282d9d2a2e0bfed3e189973bd4861f958143f0e
Thomas Micol <thomasmicol@gmail.com>
2019-06-17 18:39:36 +1200

    Bridge Pattern: Implemented the screws with hardhat implementor

--- Bridge Pattern/Implementor/ScrewsWithHardhat.cs
+++ Bridge Pattern/Implementor/ScrewsWithHardhat.cs
@@ -6,5 +6,31 @@ namespace Bridge_Pattern.Implementor
 {
     class ScrewsWithHardhat : IComboDeal
     {
+        private List<Item> _myItems;
+
+        public ScrewsWithHardhat()
+        {
+            _myItems = new List<Item> {
+                new Item {
+                    Name = "Philips head screw",
+                    Price = 0.50f,
+                    Description = "a pack of philips head screws"
+                },
+                new Item {
+                    Name = "Flat head screw",
+                    Price = 0.40f,
+                    Description = "a pack of flat head screws"
+                },
+                new Item {
+                    Name = "Hardhat",
+                    Price = 2.40f,
+                    Description = "A bog standard hardhat"
+                }
+            };
+        }
+        public List<Item> GetItems()
+        {
+            return _myItems;
+        }
     }
 }
