31a56b5f5d82247d25e8e64156c79b85e833fd70
Thomas Micol <thomasmicol@gmail.com>
2019-07-02 22:37:47 +1200

    Event Aggregator: Added the project

---
+++ Event Aggregator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Event_Aggregator_Pattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
