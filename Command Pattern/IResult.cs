919af36b6eec3b30cd1ec22224a9c19e717bf9ca
Thomas Micol <thomasmicol@gmail.com>
2019-07-03 21:27:00 +1200

    Command Pattern: Added the iresult interface

---
+++ Command Pattern/IResult.cs
@@ -0,0 +1,11 @@
+﻿csusing System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Command_Pattern
+{
+    public interface IResult
+    {
+        IResult WriteResultToLine();
+    }
+}
