60fa1e4500f2a4646a8248ef05e655f3a05c7e80
Thomas Micol <thomasmicol@gmail.com>
2019-07-03 21:26:03 +1200

    Command Pattern: Added the command parser file

---
+++ Command Pattern/CommandParser.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace Command_Pattern
+{
+    public class CommandParser
+    {
+        private List<ICommand> _myCommands;
+        private Character _myCharacter;
+        public CommandParser()
+        {
+            myCommands = new List<ICommand>
+            {
+                new AddMoneyCommand(),
+                new AddExperienceCommand(),
+                new 
+            }
+        }
+
+
+    }
+}
