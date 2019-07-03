42815fce3c34a2cd88b570e8ae79c5eb5ecfe86c
Thomas Micol <thomasmicol@gmail.com>
2019-07-03 22:41:08 +1200

    Command Pattern: Added the add experience command

---
+++ Command Pattern/AddExperienceCommand.cs
@@ -0,0 +1,35 @@
+﻿namespace Command_Pattern
+{
+    internal class AddExperienceCommand : ICommand, IChainable
+    {
+        public ICommand _nextLink { get; set; }
+        public string CommandString { get; set; } = "add experience";
+
+        public IResult Execute(Character aChar, string aCommand)
+        {
+            if(aCommand != CommandString)
+            {
+                return _nextLink.Execute(aChar, aCommand);
+            }
+            else
+            {
+                aChar.exerience += 50;
+                return BuildResult();
+            }
+        }
+
+        public IChainable SetNext(ICommand next)
+        {
+            _nextLink = next;
+            return next;
+        }
+
+        protected IResult BuildResult()
+        {
+            return new Result
+            {
+                _myResultText = "50 Experience was added"
+            };
+        }
+    }
+}
