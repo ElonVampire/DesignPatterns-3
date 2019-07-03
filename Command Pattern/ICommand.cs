d1c5799f1e3def37e35a04afd479a3631d0be47d
Thomas Micol <thomasmicol@gmail.com>
2019-07-03 22:45:01 +1200

    Command Pattern: altered the command and result interfaces

--- Command Pattern/ICommand.cs
+++ Command Pattern/ICommand.cs
@@ -1,6 +1,9 @@
 ﻿namespace Command_Pattern
 {
-    internal interface ICommand
+    internal interface ICommand : IChainable
     {
+        string CommandString { get; set; }
+        IResult Execute(Character aChar, string aaCommand);
+
     }
 }
--- Command Pattern/IResult.cs
+++ Command Pattern/IResult.cs
@@ -1,4 +1,4 @@
-﻿csusing System;
+﻿using System;
 using System.Collections.Generic;
 using System.Text;
 
