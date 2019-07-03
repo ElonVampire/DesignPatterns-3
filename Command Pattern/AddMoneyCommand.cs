98dbb90c94d2b0c0144dbc93c13b29f224ab29c7
Thomas Micol <thomasmicol@gmail.com>
2019-07-03 22:41:29 +1200

    Command Pattern: added the add money command

---
+++ Command Pattern/AddMoneyCommand.cs
@@ -0,0 +1,35 @@
+﻿namespace Command_Pattern
+{
+    internal class AddMoneyCommand : ICommand, IChainable
+    {
+        public ICommand _nextLink { get; set; }
+        public string CommandString { get; set; } = "add money";
+
+        public IResult Execute(Character aChar, string aCommand)
+        {
+            if (aCommand != CommandString)
+            {
+                return _nextLink.Execute(aChar, aCommand);
+            }
+            else
+            {
+                aChar.money += 50;
+                return BuildResult();
+            }
+        }
+
+        public IChainable SetNext(ICommand next)
+        {
+            _nextLink = next;
+            return next;
+        }
+
+        protected IResult BuildResult()
+        {
+            return new Result
+            {
+                _myResultText = "50 money was added"
+            };
+        }
+    }
+}
