c108b3f794e275c6d03ac63f0484f9c691c3fe60
Thomas Micol <thomasmicol@gmail.com>
2019-07-03 22:44:02 +1200

    Command Pattern: added the concrete result class

---
+++ Command Pattern/Result.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Command_Pattern
+{
+    internal class Result : IResult
+    {
+        public string _myResultText { get; set; }
+
+        public IResult WriteResultToLine()
+        {
+            Console.WriteLine(_myResultText);
+            return this;
+        }
+    }
+}
