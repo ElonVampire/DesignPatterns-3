444b14354e99c6adfb7849c2384acef90cac7444
Thomas Micol <thomasmicol@gmail.com>
2019-07-03 22:42:51 +1200

    Command Pattern: added the end of chain null item

---
+++ Command Pattern/EndOfChainItem.cs
@@ -0,0 +1,43 @@
+﻿namespace Command_Pattern
+{
+    internal class EndOfChainItem : ICommand, IChainable
+    {
+        public string CommandString { get; set; } = null;
+        public ICommand _nextLink { get; set; }
+
+        public IResult Execute(Character aChar, string aaCommand)
+        {
+            if(aaCommand == "exit")
+            {
+                Program.isExitFlag = true;
+                return BuildExitResult();
+            }
+            else
+            {
+                return BuildResult();
+            }
+        }
+
+        public IChainable SetNext(ICommand next)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        protected IResult BuildResult()
+        {
+            return new Result()
+            {
+                _myResultText = "There was no such command"
+            };
+        }
+
+        protected IResult BuildExitResult()
+        {
+            return new Result()
+            {
+                _myResultText = "BYE!"
+            };
+
+        }
+    }
+}
