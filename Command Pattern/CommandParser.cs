952f7428c8492532a6363b5cae48c681c1001963
Thomas Micol <thomasmicol@gmail.com>
2019-07-03 22:42:32 +1200

    Command Pattern: edited the command parser to include the show command

--- Command Pattern/CommandParser.cs
+++ Command Pattern/CommandParser.cs
@@ -2,20 +2,36 @@
 
 namespace Command_Pattern
 {
-    public class CommandParser
+    public class 
+        CommandParser
     {
         private List<ICommand> _myCommands;
         private Character _myCharacter;
         public CommandParser()
         {
-            myCommands = new List<ICommand>
+            _myCharacter = new Character();
+            _myCommands = new List<ICommand>
             {
                 new AddMoneyCommand(),
                 new AddExperienceCommand(),
-                new 
-            }
+                new RemoveMoneyCommand(),
+                new RemoveExperienceCommand(),
+                new ShowStatsCommand()
+            };
+
+            _myCommands[0].SetNext(_myCommands[1])
+                .SetNext(_myCommands[2])
+                .SetNext(_myCommands[3])
+                .SetNext(_myCommands[4])
+                .SetNext(new EndOfChainItem());
+            
         }
 
+
+        public IResult ParseCommand(string command)
+        {
+            return _myCommands[0].Execute(_myCharacter, command);
+        }
 
     }
 }
