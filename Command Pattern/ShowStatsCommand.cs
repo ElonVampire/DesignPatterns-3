95ac81a5c4c057737ba2e0a69a3685bb6658ca94
Thomas Micol <thomasmicol@gmail.com>
2019-07-03 22:44:35 +1200

    Command Pattern: added the show stats command

---
+++ Command Pattern/ShowStatsCommand.cs
@@ -0,0 +1,34 @@
+﻿namespace Command_Pattern
+{
+    internal class ShowStatsCommand : ICommand, IChainable
+    {
+        public ICommand _nextLink { get; set; }
+        public string CommandString { get; set; } = "show";
+
+        public IResult Execute(Character aChar, string aCommand)
+        {
+            if (aCommand != CommandString)
+            {
+                return _nextLink.Execute(aChar, aCommand);
+            }
+            else
+            {
+                return BuildResult(aChar);
+            }
+        }
+
+        public IChainable SetNext(ICommand next)
+        {
+            _nextLink = next;
+            return next;
+        }
+
+        protected IResult BuildResult(Character aChar)
+        {
+            return new Result
+            {
+                _myResultText = string.Format("This character currently has {0} money and {1} experience", aChar.money, aChar.exerience)
+            };
+        }
+    }
+}
