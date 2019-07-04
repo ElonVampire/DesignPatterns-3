420332983056515f3232eb31ca0630bbeea99a88
Thomas Micol <thomasmicol@gmail.com>
2019-07-04 22:22:22 +1200

    Command Pattern: added a command builder

---
+++ Composite Pattern/Commands/CommandBuilder/ConcreteCommandBuilders/AddGroupCommandBuilder.cs
@@ -0,0 +1,11 @@
+﻿namespace Composite_Pattern
+{
+    internal class AddGroupCommandBuilder : IBuilder
+    {
+        public ICommand BuildCommand()
+        {
+            ICommand result = new AddGroupCommand();
+            return result;
+        }
+    }
+}
