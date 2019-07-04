2f1384e4a9303483b62c642aa04cf67b3f8b18ee
Thomas Micol <thomasmicol@gmail.com>
2019-07-04 22:23:35 +1200

    Command Pattern: added the adventurer command maker class

---
+++ Composite Pattern/Commands/CommandBuilder/AdventurerCommandsMaker.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+
+namespace Composite_Pattern
+{
+    internal class AdventurerCommandsMaker : ICommandMaker
+    {
+        private List<ICommand> _builtCommands;
+        private List<IBuilder> _myBuilders;
+
+        public AdventurerCommandsMaker()
+        {
+            _myBuilders = new List<IBuilder>
+            {
+                new AddPartyMemberCommandBuilder(),
+                new RemovePartyMemberCommandBuilder(),
+                new AddGroupCommandBuilder(),
+                new RemoveGroupCommandBuilder(),
+                new AddExperienceCommandBuilder(),
+                new AddMoneyCommandBuilder(),
+                new RemoveMoneyCommandBuilder(),
+                new PrettyPrintPartyStatsCommandBuilder(),
+                new NullEndLineCommandBuilder()
+            };
+        }
+
+        public void BuildCommands()
+        {
+            _builtCommands = new List<ICommand>();
+            foreach(IBuilder builder in _myBuilders)
+            {
+                _builtCommands.Add(builder.BuildCommand());
+            }
+        }
+
+        public List<ICommand> GetCommands()
+            => _builtCommands != null ? _builtCommands : null;
+
+    }
+}
