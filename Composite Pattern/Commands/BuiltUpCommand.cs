7c2566a057aa909dd04bd10adc85f4278625056a
Thomas Micol <thomasmicol@gmail.com>
2019-07-04 22:27:21 +1200

    Command Pattern: added the builtupcommand data model class

---
+++ Composite Pattern/Commands/BuiltUpCommand.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace Composite_Pattern
+{
+    internal class BuiltUpCommand : IRegisteredCommand
+    {
+        public string BaseCommandText { get; set; }
+        public List<string> Parameters {get; set;}
+    }
+}
