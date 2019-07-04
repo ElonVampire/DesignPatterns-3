2f536bc620416683f3be7db26ffd4c19830bf6f9
Thomas Micol <thomasmicol@gmail.com>
2019-07-04 22:21:36 +1200

    Command Pattern: added a command builder

---
+++ Composite Pattern/Commands/CommandBuilder/ConcreteCommandBuilders/AddExperienceCommandBuilder.cs
@@ -0,0 +1,11 @@
+﻿namespace Composite_Pattern
+{
+    internal class AddExperienceCommandBuilder : IBuilder
+    {
+        public ICommand BuildCommand()
+        {
+            ICommand result = new AddExperienceCommand();
+            return result;
+        }
+    }
+}
