5e582dbb1afdb28c0d54c5b969a66e37615b31b8
Thomas Micol <thomasmicol@gmail.com>
2019-07-04 22:26:43 +1200

    Command Pattern: added a null object class to end the CoR command chain without error and also added the base abstract class base chainable which defines base behaviour for items that reside in the CoR

---
+++ Composite Pattern/Commands/ConcreteCommands/BaseChainable.cs
@@ -0,0 +1,14 @@
+﻿namespace Composite_Pattern
+{
+    internal abstract class BaseChainable : IChainable
+    {
+        public ICommand _NextCommand { get; set; }
+        public bool isEndItem { get; set; }
+
+        public virtual ICommand SetNext(ICommand aCommand)
+        {
+            _NextCommand = aCommand;
+            return _NextCommand;
+        }
+    }
+}
---
+++ Composite Pattern/Commands/ConcreteCommands/NullEndLineCommand.cs
@@ -0,0 +1,29 @@
+﻿namespace Composite_Pattern
+{
+    internal class NullEndLineCommand : BaseChainable, ICommand
+    {
+        public string CommandText { get; set; }
+
+        public NullEndLineCommand()
+        {
+            isEndItem = true;
+        }
+
+        public IResult Execute(PartyModel repo, IRegisteredCommand comm)
+        {
+            string mes = "Command of " + comm.BaseCommandText;
+            if(comm.Parameters != null)
+            {
+                mes += " with the parameters of ";
+                foreach (var i in comm.Parameters)
+                {
+                    mes += i + ", ";
+                }
+            }
+            
+            mes += " was not found please try again";
+             return new Result(mes);
+        }
+
+    }
+}
