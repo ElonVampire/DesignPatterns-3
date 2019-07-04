25762ea72d7d60d3df316e9e4b4706d82b86f4d5
Thomas Micol <thomasmicol@gmail.com>
2019-07-04 22:24:46 +1200

    Command Pattern: added the result model class

---
+++ Composite Pattern/Commands/ConcreteCommands/Result.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Composite_Pattern
+{
+    internal class Result : IResult
+    {
+        private string _myMessage;
+
+        public Result(string message)
+        {
+            _myMessage = message;
+        }
+
+        public Result()
+        {
+            _myMessage = "";
+        }
+
+        public void EvaulateToCommandLine()
+        {
+            Console.WriteLine(_myMessage);
+        }
+    }
+}
