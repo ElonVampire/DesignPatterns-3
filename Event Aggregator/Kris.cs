6781b5a0209bd7b11530bde452f8a33a5a3d177e
Thomas Micol <thomasmicol@gmail.com>
2019-09-02 21:46:22 +1200

    added concrete subscribers

---
+++ Event Aggregator/Kris.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Event_Aggregator_Pattern
+{
+    public class Kris : ISubscriber<GoalScored>
+    {
+        public string name { get; set; } = "Kris";
+
+        public Kris(IEventAggregator ea)
+        {
+            ea.Subscribe(this);
+        }
+
+        public void OnEvent(GoalScored e)
+        {
+            Console.WriteLine($"{name} sees that {e.WhatHappened()}");
+        }
+    }
+}
---
+++ Event Aggregator/Thomas.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Event_Aggregator_Pattern
+{
+    public class Thomas : ISubscriber<BallKicked>, ISubscriber<GoalScored>
+    {
+        public string name { get; set; } = "Thomas";
+
+        public Thomas(IEventAggregator ea)
+        {
+            ea.Subscribe(this);
+        }
+
+        public void OnEvent(GoalScored e)
+        {
+            Console.WriteLine($"{name} sees that {e.WhatHappened()}");
+        }
+
+        public void OnEvent(BallKicked e)
+        {
+            Console.WriteLine($"{name} sees that {e.WhatHappened()}");
+        }
+    }
+}
